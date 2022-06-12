eadb66dff6a21f6da830a16597f69abb210ba7de
Kota <maedakoutaa@gmail.com>
2022-06-12 14:51:39 +0900

    Initial Commit

---
+++ MiniApp55-FireBase/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MiniApp55-FireBase
+//
+//  Created by 前田航汰 on 2022/06/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
