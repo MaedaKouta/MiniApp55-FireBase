9b6ecc7c0ba15c83806841815d1eff4812c22580
Kota <maedakoutaa@gmail.com>
2022-06-14 10:28:43 +0900

    メール認証の導入

--- MiniApp55-FireBase/SignUpViewController.swift
+++ MiniApp55-FireBase/SignUpViewController.swift
@@ -13,6 +13,11 @@ class SignUpViewController: UIViewController {
     @IBOutlet weak var mailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        Auth.auth().languageCode = "ja_JP"
+    }
+
     @IBAction func didTapSingnUpButton(_ sender: Any) {
         let mail = mailTextField.text ?? ""
         let password = passwordTextField.text ?? ""
@@ -21,6 +26,7 @@ class SignUpViewController: UIViewController {
 
     func signUp(_ mail: String, _ password: String) {
         Auth.auth().createUser(withEmail: mail, password: password) { authResult, error in
+
             if let error = error as NSError?, let errorCode = AuthErrorCode.Code(rawValue: error.code) {
                 switch errorCode {
                 case .invalidEmail:
@@ -32,13 +38,20 @@ class SignUpViewController: UIViewController {
                 default:
                     print(error.domain)
                 }
-                if let user = authResult?.user {
-                    dump(user)
-                    print("登録成功")
-                } else {
-                    dump(error)
-                }
             }
+
+            // TODO: メール認証しなくてもアカウント作れてしまう。なぜ...
+            if let user = authResult?.user {
+                print("通貨")
+                user.sendEmailVerification(completion: { error in
+                    if error == nil {
+                        print("送信成功しました")
+                    } else {
+                        print("メール失敗")
+                    }
+                })
+            }
+
         }
     }
 
